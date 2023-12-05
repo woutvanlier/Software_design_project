f5c81e595c2196456c456041101fd92f3baac9cf
ZioltZe <eliott.vandenhoudt@gmail.com>
2023-12-05 12:21:44 +0100

    Test

--- JavaProject/MoneyTracker/src/main/java/UI/FrameManager.java
+++ JavaProject/MoneyTracker/src/main/java/UI/FrameManager.java
@@ -8,5 +8,33 @@ import javax.swing.*;
 import java.awt.*;
 
 public class FrameManager extends JFrame {
-    
+    public FrameManager() {
+        super("Splitwise");
+    }
+
+    Person_database personDatabase;
+    Ticket_database ticketDatabase;
+    public Person_database getPersonDatabase() {
+        return personDatabase;
+    }
+    public Ticket_database getTicketDatabase() {
+        return ticketDatabase;
+    }
+
+    public void initialize(Person_database personDatabase, Ticket_database ticketDatabase)
+    {
+        this.personDatabase = personDatabase;
+        this.ticketDatabase = ticketDatabase;
+
+        HomeView homeView = new HomeView(this, personDatabase, ticketDatabase);
+
+        this.setSize(350, 650);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.getContentPane().setBackground(new Color(30, 30, 30));
+
+        this.setContentPane(homeView);
+
+        this.setVisible(true);
+    }
 }
