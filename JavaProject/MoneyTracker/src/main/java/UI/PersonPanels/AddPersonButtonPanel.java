4f5f3333c90e25e52f866c50fd6069f2a15641bb
samotov <150435208+samotov@users.noreply.github.com>
2023-12-16 13:31:14 +0100

    UI update

--- JavaProject/MoneyTracker/src/main/java/UI/PersonPanels/AddPersonButtonPanel.java
+++ JavaProject/MoneyTracker/src/main/java/UI/PersonPanels/AddPersonButtonPanel.java
@@ -10,9 +10,13 @@ import database.Person_database;
 
 import javax.swing.*;
 import javax.xml.transform.Result;
+import  UI.ButtonSound;
 
 public class AddPersonButtonPanel extends JPanel
 {
+
+    ButtonSound buttonSound = new ButtonSound();
+
     public AddPersonButtonPanel(PersonPanel personPanel, JTextField personNameField, Person_database personDatabase, PersonView personView)
     {
         JButton button = new JButton("ADD USER");
@@ -32,6 +36,8 @@ public class AddPersonButtonPanel extends JPanel
             else
             {
                 personView.setError(true);
+                buttonSound.playSound("src/main/java/UI/error.wav");
+
             }
         });
         this.add(button);
